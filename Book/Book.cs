584f2595380ded1f8fbe6cdf790ca677d5b59497
Utkir <utkir-0710@mail.ru>
2025-04-25 11:11:01 +0500

    Add files via upload

---
+++ Book/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book;
+
+public class Book
+{
+    private string _author;
+    private decimal _price;
+    
+    public string Author
+    {
+        get { return _author; }
+        set { _author = value; }
+    }
+    public decimal Price
+    {
+        get { return _price; }
+        set { _price = value; }
+    }
+    public string Name { get; set; }
+    public int PublishYear { get; set; }
+    public string Genre { get; set; }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Book name: {Name}");
+        Console.WriteLine($"It's author: {Author}");
+        Console.WriteLine($"Price of book: {Price}");
+        Console.WriteLine($"Publish year: {PublishYear}");
+        Console.WriteLine(($"Genre: {Genre}"));
+    }
+    
+    public void DiscountPrice(decimal percentage)
+    {
+        if (percentage > 0 && percentage < 100)
+        {
+            _price = _price * (percentage / 100);
+        }
+    }
+}
+    
---
+++ Book/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book()
+            {
+                Name = "Atomic Habits",
+                Author = "James Clear",
+                Price = 45,
+                Genre = "Self-help",
+                PublishYear = 2018
+            };
+
+            book.DisplayInfo();
+            book.DiscountPrice(50);
+            Console.WriteLine();
+            book.DisplayInfo();
+        }
+    }
+}
